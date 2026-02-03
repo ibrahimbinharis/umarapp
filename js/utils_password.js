566007860b80d550a6c4f30fb590892e1ea89e77
ibrahimbinharis <73592957+ibrahimbinharis@users.noreply.github.com>
2026-02-04 01:33:36 +0700

    Delete js directory

--- js/utils_password.js
+++
@@ -1,6 +0,0 @@
-function togglePasswordVisibility(inputId, btn) {
-    const input = document.getElementById(inputId);
-    const isPassword = input.type === 'password';
-    input.type = isPassword ? 'text' : 'password';
-    btn.querySelector('span').innerText = isPassword ? 'visibility_off' : 'visibility';
-}
