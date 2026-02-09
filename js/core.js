69d6055be4f8d8f7954fc728026b547d02034637
ibrahimbinharis <73592957+ibrahimbinharis@users.noreply.github.com>
2026-02-09 13:58:13 +0700

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
 // --- 1. CONFIG & DATABASE (SUPABASE v36) ---
 const APP_CONFIG = {
     appName: "E-Umar",
-    version: "v36-supabase",
+    version: "v2.0",
     supabaseUrl: "https://fxtmilqvxomuvkxxzjli.supabase.co",
     supabaseKey: "sb_publishable_aXcK3znrtRo0d3gH-Wg1Ew_-0Z3262O"
 };
@@ -410,3 +410,4 @@ function formatDateLong(isoString, timeOverride = null) {
 
     return `${datePart}<br><span class="text-xs text-slate-400 font-normal">${timePart}</span>`;
 }
+
