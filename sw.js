3c8a25c04404085ff4030eeb1ffd18305637b728
ibrahimbinharis <73592957+ibrahimbinharis@users.noreply.github.com>
2026-02-10 08:15:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "v2.1";
+const CACHE_NAME = "v2.2";
 const ASSETS_TO_CACHE = [
     "./",
     "./index.html",
@@ -86,3 +86,4 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+
