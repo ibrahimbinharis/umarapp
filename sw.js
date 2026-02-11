86351fdaee511338aa49a9f538b1f75b70e01164
ibrahimbinharis <73592957+ibrahimbinharis@users.noreply.github.com>
2026-02-12 00:41:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "e-umar-v2";
+const CACHE_NAME = "e-umar-v3";
 const ASSETS_TO_CACHE = [
     "./",
     "./index.html",
@@ -124,5 +124,6 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+
 
 
