b524993717d681a41a7ac1006d16d648601bcfe6
ibrahimbinharis <73592957+ibrahimbinharis@users.noreply.github.com>
2026-02-17 08:10:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "v2.0";
+const CACHE_NAME = "v2.1";
 const ASSETS_TO_CACHE = [
     "./",
     "./index.html",
@@ -185,3 +185,4 @@ self.addEventListener('notificationclick', (event) => {
         })
     );
 });
+
