31d14fc5eb4dad3712995358aa52a82523dd7bd8
ibrahimbinharis <73592957+ibrahimbinharis@users.noreply.github.com>
2026-03-07 01:43:15 +0700

    Add files via upload

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
 // --- 1. CONFIG & DATABASE (SUPABASE v37) ---
 const APP_CONFIG = {
     appName: "E-Umar",
-    version: "v4.2",
+    version: "v4.3",
     supabaseUrl: "https://fxtmilqvxomuvkxxzjli.supabase.co",
     supabaseKey: "sb_publishable_aXcK3znrtRo0d3gH-Wg1Ew_-0Z3262O"
 };
